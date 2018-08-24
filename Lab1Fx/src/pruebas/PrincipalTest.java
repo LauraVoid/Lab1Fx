1e4e13703ba36316e3ef91ba23e8429f488d766d
Douglas <Douglas@172.30.177.177>
2018-08-23 20:42:27 -0500

    no terminamos :( el tiempo no nos dió, ojalá nos den más tiempo... profe si lees esto por favor denos mas tiempo

--- Lab1Fx/src/pruebas/PrincipalTest.java
+++ Lab1Fx/src/pruebas/PrincipalTest.java
@@ -1,14 +1,57 @@
 package pruebas;
+import modelo.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class PrincipalTest {
 
+	//Attributes 
+	private Principal principal; 
+	
+	@Before
+	public void before() {
+		principal = new Principal();
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testVerificateIntegerDouble() {
+		principal = new Principal();
+		boolean isTrue= false;
+		String code = "1";
+	    boolean actually = principal.verificateIntegerDouble(code);
+	  
+		assertEquals(isTrue, actually);
+	}
+	
+	@Test
+	public void testCalculatePercent() {
+		principal = new Principal();
+		int number = 50;
+		int percent = 50;
+	    int hope = 25;
+	    int actually = principal.calculatePercent(number, percent);
+	    
+	    assertEquals(hope, actually);
+		
+	}
+	
+	@Test
+	public void testRandomInt() {
+		principal = new Principal();
+		int amount = 4;
+		boolean repeated = true;
+		int LowerIndex= 1;
+		int HighIndex= 5;
+		
+		ArrayList<Integer> inter = principal.randomInt(amount, repeated, LowerIndex, HighIndex);
+		for(int a: inter) {
+			System.out.println(a);
+		}
 	}
 
 }
